69581365ed11911b87d9eb05559d21d89b17af1f
jiujieti <melody2014ymq@gmail.com>
2017-11-13 20:54:02 +0100

    add two apis

--- test/test.js
+++ test/test.js
@@ -1,4 +1,21 @@
 var Client = require('../lib/index');
 
 var client = new Client('f0fa0a19', '136dd9a4ce628d307cb0fbbc89df8c66');
-client.findAll();
+// client.findFlights().then(
+//   (data) => {
+//     console.log(data);
+//   }
+// );
+
+// var options = {
+//   airline: 'KL'
+// };
+// client.findFlights(options).then(
+//   (data) => {
+//     console.log(data);
+//   }
+// );
+
+ client.findOneFlightById(123063231720956892).then((data) => {console.log(data)}); 
+
+// client.findOneFlightById().then((data) => {console.log(data)}).catch((e) => {console.log(e)}); 
