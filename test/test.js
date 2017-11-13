3392e84eea4b5c704b57626188d01d749b9ac5bf
jiujieti <melody2014ymq@gmail.com>
2017-11-13 21:29:13 +0100

    apply camel style

--- test/test.js
+++ test/test.js
@@ -16,6 +16,6 @@ var client = new Client('f0fa0a19', '136dd9a4ce628d307cb0fbbc89df8c66');
 //   }
 // );
 
- client.findOneFlightById(123063231720956892).then((data) => {console.log(data)}); 
+// client.findOneFlightById(123063231720956892).then((data) => {console.log(data)}); 
 
 // client.findOneFlightById().then((data) => {console.log(data)}).catch((e) => {console.log(e)}); 
